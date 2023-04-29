cf122c284b4352f904f7dd9362f0dd3c210e42d3
Zoran Simonovic <simonovic1980@gmail.com>
2023-04-29 20:19:13 +0200

    fetch data from google and show like title

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
-function WebSearchPage() {
-  return <div>WebSearchPage</div>;
+async function WebSearchPage({ searchParams }) {
+  const response = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}}`
+  );
+
+  const data = await response.json();
+
+  console.log(data);
+
+  const results = data.items;
+  return (
+    <>
+      {results &&
+        results.map((result) => (
+          <h1 key={result.displayLink}>{result.title}</h1>
+        ))}
+    </>
+  );
 }
 
 export default WebSearchPage;
