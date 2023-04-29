ad62978bef46d77e6c48524a4ca86202dddb5a6e
Zoran Simonovic <simonovic1980@gmail.com>
2023-04-29 21:10:51 +0200

    create search result component

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -4,7 +4,7 @@ import "./globals.css";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className="relative min-h-screen">
         {children}
         <Footer />
       </body>
--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+import WebSearchResults from "@/components/WebSearchResults";
 import Link from "next/link";
 
 async function WebSearchPage({ searchParams }) {
+  await new Promise((resolve) => setTimeout(resolve, 10000));
   const response = await fetch(
     `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}}`
   );
 
   if (!response.ok) {
+    console.log(response);
     throw new Error("Something went wrong");
   }
 
@@ -27,14 +30,7 @@ async function WebSearchPage({ searchParams }) {
     );
   }
 
-  return (
-    <>
-      {results &&
-        results.map((result) => (
-          <h1 key={result.displayLink}>{result.title}</h1>
-        ))}
-    </>
-  );
+  return <>{results && <WebSearchResults results={data} />}</>;
 }
 
 export default WebSearchPage;
